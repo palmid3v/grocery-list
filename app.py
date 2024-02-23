adbdef62a2f4de3d60763516db14b22be713d4e5
palmibb <cpalmi0725@gmail.com>
2024-02-23 00:47:40 -0500

    working with postgres - updated new database

--- app.py
+++ app.py
@@ -7,31 +7,16 @@ app = Flask(__name__)
 conn = psycopg2.connect(
     host='localhost',
     port='5432',
-    database='lista_compras',
+    database='grocery_shopping',
     user='postgres',
     password='Paaccaal96#!.'
 )
 
-# this option is optional, you can create it before running code and simply comment this part or just remove it
-def create_table():
-    cursor = conn.cursor()
-    cursor.execute("""
-        CREATE TABLE IF NOT EXISTS compras (
-            id SERIAL PRIMARY KEY,
-            item VARCHAR(255),
-            kg FLOAT
-        )
-    """)
-    conn.commit()
-    cursor.close()
-
-# Call this function to create the table
-create_table()
-
+# you should create first=database then tabble
 
 def check_database():
     cursor = conn.cursor()
-    cursor.execute("SELECT item, kg FROM compras")
+    cursor.execute("SELECT item, kg FROM groceries")
     existing_items = cursor.fetchall()
     cursor.close()
     if existing_items:
@@ -42,21 +27,21 @@ def check_database():
 
 def clear_database():
     cursor = conn.cursor()
-    cursor.execute("DELETE FROM compras")
+    cursor.execute("DELETE FROM groceries")
     conn.commit()
     cursor.close()
 
 
 def insert_item(item, kg):
     cursor = conn.cursor()
-    cursor.execute("INSERT INTO compras (item, kg) VALUES (%s, %s)", (item, kg))
+    cursor.execute("INSERT INTO groceries (item, kg) VALUES (%s, %s)", (item, kg))
     conn.commit()
     cursor.close()
 
 
 def remove_item(item):
     cursor = conn.cursor()
-    cursor.execute("DELETE FROM compras WHERE item = %s", (item,))
+    cursor.execute("DELETE FROM groceries WHERE item = %s", (item,))
     conn.commit()
     cursor.close()
 
@@ -64,7 +49,8 @@ def remove_item(item):
 @app.route('/')
 def index():
     existing_items = check_database()
-    return render_template('index.html', items=existing_items)
+    return render_template('index.html', items=existing_items, message=request.args.get('message'))  # Updated this line
+
 
 
 @app.route('/add', methods=['POST'])
@@ -75,7 +61,7 @@ def add_item():
     existing_items = check_database()
 
     if item_compra in [item[0] for item in existing_items]:
-        return "Item already exists in the list."
+        return redirect(url_for('index', message='Item already exists in the list.'))  # Updated this line
 
     try:
         kg_compra = float(kg_compra)
@@ -84,6 +70,8 @@ def add_item():
 
     insert_item(item_compra, kg_compra)
     return redirect(url_for('index'))
+
+
 
 
 @app.route('/remove', methods=['POST'])
